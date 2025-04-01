87426aeb81ad6ee83fec93c2bebac5c2387cd15d
Helder <hcbfilho@hotmail.com>
2025-04-01 13:56:43 -0300

    mais exercicios

---
+++ Exercicio-3.py
@@ -0,0 +1,4 @@
+n1 = float(input("Insira o primeiro número"))
+n2 = float(input("Insira o segundo número"))
+
+print(f'A soma entre os números = {n1+n2}')
---
+++ aula6e.py
@@ -0,0 +1,4 @@
+n = input('Digite algo:')
+
+print(n.isalnum()) # Para saber se o valor de 'n' é alfa numérico
+                   # Existem vários comandos com is....
